b659d0653db38c558d06e75e9ed95b187a6509c7
prudhviraj310 <prudhviraj7675@gmail.com>
2026-04-06 18:33:12 +0530

    Fix: Update database host to 'food-app-db' for container networking

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db =  mysql.createConnection({
-  host: "127.0.0.1",
+  DB_HOST=food-app-db,
   user: 'root',
   password: 'Prudhviraj@310',
   database: 'fooddb'
